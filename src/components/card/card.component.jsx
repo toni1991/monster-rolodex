38206263cb181131b9bed6007dd6c0cfb2e5ca54
Antonio Grieco <grieco@itestra.com>
2021-08-30 18:48:36 +0200

    Files.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = ({monster}) => (
     <div className='card-container' onClick={(e) => alert(monster.name.toLowerCase())}>
-        <img alt="monster" src={`https://robohash.org/${monster.id}?set=set5&size=180x180`} />
+        <img alt="monster" src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
         <h2>{monster.name}</h2>
         <p>{monster.email}</p>
     </div>
